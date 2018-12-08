87a928aa1bf81b67305d3b22c744ef26293984d9
The Flying Nomad <TheFlyingNomad@domain.com>
2018-12-07 19:50:43 -0800

    Move out utils into a separate projects

--- Contracts/App.go
+++ Contracts/App.go
@@ -3,12 +3,10 @@ package Contracts
 import (
 	"fmt"
 	"net/http"
-
-	"github.com/TheFlyingNomad/JerryMouse/Contracts"
 )
 
 // RenderTemplate -
-func RenderTemplate(w http.ResponseWriter, appContext *Contracts.ServerAppContext, templateFile string, templateData interface{}) {
+func RenderTemplate(w http.ResponseWriter, appContext *ServerAppContext, templateFile string, templateData interface{}) {
 	executeError := appContext.Templates.ExecuteTemplate(w, templateFile, templateData)
 	if executeError != nil {
 		fmt.Printf("RenderTemplate: %s", executeError)
